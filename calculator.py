d5f2dc3c14f6896dc0e3e3bea44d44951d1793d2
saini rahul <sainirahul4148@gmail.com>
2025-04-11 11:43:04 +0530

    calci added

---
+++ calculator.py
@@ -0,0 +1,28 @@
+def add(a, b):
+    """
+    Returns the sum of a and b.
+    """
+    # TODO: Implement addition
+    pass
+
+def subtract(a, b):
+    """
+    Returns the result of subtracting b from a.
+    """
+    # TODO: Implement subtraction
+    pass
+
+def multiply(a, b):
+    """
+    Returns the product of a and b.
+    """
+    # TODO: Implement multiplication
+    pass
+
+def divide(a, b):
+    """
+    Returns the result of dividing a by b.
+    Raise ValueError if b is zero.
+    """
+    # TODO: Implement division with error handling
+    pass
